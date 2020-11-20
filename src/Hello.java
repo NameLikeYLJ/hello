dc0ab39a5e917b0092f9fbda5877b257cd2277e7
NameLikeYLJ <645785798@qq.com>
2020-11-20 15:17:08 +0800

    关心则乱

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("3th");
         System.out.println("我是上传的版本121221!");
         System.out.println("我是新push的版本!");
+        System.out.println("关心则乱!");
     }
 }
