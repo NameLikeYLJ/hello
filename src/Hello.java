fdf44aa0ab8bc835ce87d543d6a145a4b966eabe
NameLikeYLJ <645785798@qq.com>
2020-11-20 13:19:22 +0800

    test新push

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("3th");
         System.out.println("我是上传的版本121221!");
+        System.out.println("我是新push的版本!");
     }
 }
